7263fe081b864a91e2cfdf23cceed791fc2f8394
Collins Asega <collinsasega.ca@gamil.com>
2026-02-09 20:46:04 +0300

    handle static files, storage of persistent data to the db

--- geodatamanager/settings/base.py
+++ geodatamanager/settings/base.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = LOCAL_APPS + THIRD_PARTY_APPS + [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -178,11 +179,14 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    PROJECT_DIR / "static",
+    os.path.join(PROJECT_DIR, "static")
 ]
 
-STATIC_ROOT = BASE_DIR / "static"
+
 STATIC_URL = "/static/"
+
+STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
